d74e36ed50ea18e4b2d21fd2a780b7633521cf8e
Rodrigo Benini <rodrigobenini15@gmail.com>
2022-06-14 19:54:42 -0300

    Delete backend directory

--- backend/src/auth/types.ts
+++
@@ -1,4 +0,0 @@
-export type Admin = {
-    user:string;
-    password:string;
-}
--- backend/src/biblioteca/biblioteca.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { BibliotecaService } from './biblioteca.service';
-import { BibliotecaController } from './biblioteca.controller';
-
-@Module({
-  controllers: [BibliotecaController],
-  providers: [BibliotecaService]
-})
-export class BibliotecaModule {}
